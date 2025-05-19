00af9bfdd5f1fd7348ed8ecb817fed7254b87ecc
enzopavani <tijoloenso@gmail.com>
2025-05-19 14:27:14 -0300

    Moving red circle

--- main.py
+++ main.py
@@ -4,21 +4,34 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
+# delta time, used for frame rate independent movement
+dt = 0
+
+playerPos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 while running:
-    # Press X to close the window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # Fill the screen with black
     screen.fill((0, 0, 0))
 
-    # Game rendering
+    pygame.draw.circle(screen, (255, 0, 0), playerPos, 40)
 
-    # Update the display
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        playerPos.y -= 300 * dt
+    if keys[pygame.K_a]:
+        playerPos.x -= 300 * dt
+    if keys[pygame.K_s]:
+        playerPos.y += 300 * dt
+    if keys[pygame.K_d]:
+        playerPos.x += 300 * dt
+    
+    if keys[pygame.K_ESCAPE]:
+        running = False
+    
     pygame.display.flip()
-    # Cap the frame rate
-    clock.tick(60)
+    dt = clock.tick(60) / 1000
 
 pygame.quit()
