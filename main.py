13c00d2960b1bde3ab4ec614b8843e1c55d75221
enzopavani <tijoloenso@gmail.com>
2025-05-19 15:00:17 -0300

    Drawing image from external resources

--- main.py
+++ main.py
@@ -2,32 +2,43 @@ import pygame
 
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
-display_suface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+displaySurface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Space Shooter")
 clock = pygame.time.Clock()
 running = True
 dt = 0
 
-playerPos = pygame.Vector2(display_suface.get_width() / 2, display_suface.get_height() / 2)
+# plain surface
+surface = pygame.Surface((100, 200))
+surface.fill('blue')
+x = 100
+
+# importing an image
+playerSurface = pygame.image.load("pygames/game0/space shooter/images/player.png")
+
+playerPosition = pygame.Vector2(displaySurface.get_width() / 2, displaySurface.get_height() / 2)
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    display_suface.fill((0, 0, 0))
+    displaySurface.fill('darkgrey')
+    # blit() puts surface on another surface
+    x += 1
+    displaySurface.blit(playerSurface, (x, 150))
 
-    pygame.draw.circle(display_suface, (255, 0, 0), playerPos, 40)
+    pygame.draw.circle(displaySurface, (255, 0, 0), playerPosition, 40)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        playerPos.y -= 300 * dt
+        playerPosition.y -= 300 * dt
     if keys[pygame.K_a]:
-        playerPos.x -= 300 * dt
+        playerPosition.x -= 300 * dt
     if keys[pygame.K_s]:
-        playerPos.y += 300 * dt
+        playerPosition.y += 300 * dt
     if keys[pygame.K_d]:
-        playerPos.x += 300 * dt
+        playerPosition.x += 300 * dt
     
     if keys[pygame.K_ESCAPE]:
         running = False
